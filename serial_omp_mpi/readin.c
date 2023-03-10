83f055bedcdfc3eb5432dbbf9c05a4030be5ea55
Sharon Yang <xiny@smu.edu>
2023-03-10 12:05:59 -0600

    Update readin.c

--- serial_omp_mpi/readin.c
+++ serial_omp_mpi/readin.c
@@ -63,12 +63,12 @@ void readin(char fname[16], char density[16]) {
 		if (strstr(buff,"ATOM")!=NULL){
 			// printf("%s \n",buff);
 			fscanf(buff,"%s %d %s %s %d %lf %lf %lf %lf %lf",&c,&i,&c,&c,&i,&a1,&a2,&a3,&b1,&b2);
-			chrpos[3*i]=a1;
-			chrpos[3*i+1]=a2;
-			chrpos[3*i+2]=a3;
-			atmchr[i]=b1;
-			atmchr[i]=b2;
-			printf("%s %i \n",c,i);
+			// chrpos[3*i]=a1;
+			// chrpos[3*i+1]=a2;
+			// chrpos[3*i+2]=a3;
+			// atmchr[i]=b1;
+			// atmchr[i]=b2;
+			// printf("%s %i \n",c,i);
 		}
 	}
 
