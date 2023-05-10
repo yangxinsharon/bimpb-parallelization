46ef037adb034babb89814d385d2837bf17c3ada
Sharon Yang <xiny@smu.edu>
2023-05-09 23:00:38 -0500

    Update treecode.c

--- treecode/treepart/treecode.c
+++ treecode/treepart/treecode.c
@@ -38,7 +38,7 @@ extern double eps;
 extern double bulk_strength;  	
 extern double kappa2;	
 extern double kappa;
-extern double **tr_xyz2D, **tr_q2D, **temp_normal, **temp_position;
+extern double **tr_xyz2D, **tr_q2D, **temp_normal, **temp_position, **matrixA;
 extern int maxparnode;
 extern int order;
 
