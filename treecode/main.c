faf2100e0c52b7a69e9ae1e4ad7a643f3f1fd144
Sharon Yang <xiny@smu.edu>
2023-05-10 15:27:14 -0500

    Update main.c

--- treecode/main.c
+++ treecode/main.c
@@ -67,8 +67,8 @@ int main(int argc, char *argv[]) {
 	work=(double *) calloc (ldw*(RESTRT+4), sizeof(double));
 	h=(double *) calloc (ldh*(RESTRT+2), sizeof(double));
 
-	// extern int TreecodeInitialization();
-	// TreecodeInitialization();
+	extern int TreecodeInitialization();
+	TreecodeInitialization();
 
 	gmres_(&N, bvct, xvct, &RESTRT, work, &ldw, h, &ldh, &iter, &resid, &matvec, &psolve, &info);
 
@@ -77,8 +77,8 @@ int main(int argc, char *argv[]) {
 	comp_soleng_wrapper(soleng); //wraps the solvation energy computation
 	timer_end();
 
-	// extern int TreecodeFinalization();
-	// TreecodeFinalization();
+	extern int TreecodeFinalization();
+	TreecodeFinalization();
 
 	/* free memory */
 	for(i=0;i<3;i++) {
