1a2a056b14653b3f683b27d551da1600be884b95
Sharon Yang <xiny@smu.edu>
2023-05-25 18:50:57 -0500

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -375,7 +375,7 @@ void lu_solve( double **matrixA, int N, int *ipiv, double *rhs ) {
 /**********************************************************/
 int *psolve(double *z, double *r) {
 /* r as original while z as scaled */
-	int i, j, jj, k;
+	int i, j, jj, k=0;
   	int idx = 0, nrow, nrow2, ibeg = 0, iend = 0;
   	int *ipiv, inc;
   	double **matrixA; 
@@ -415,13 +415,13 @@ int *psolve(double *z, double *r) {
 	    // printf("ibeg iend nrow: %d, %d, %d\n", leafarr[0][arridx], leafarr[1][arridx], leafarr[2][arridx] );
 		// printf("ibeg iend nrow is %d, %d, %d\n",ibeg,iend,nrow);
 		arridx += 1;
-		Nleafc += 1;
+		// Nleafc += 1;
 		idx += nrow;
 	}
 
 	// system("pause");
 
-  	// while ( idx < nface ) {
+  	while ( idx < nface ) {
     // 	leaflength(s_tree_root, idx);
 
     // 	nrow  = Nrow;
@@ -432,11 +432,11 @@ int *psolve(double *z, double *r) {
     	// printf("idx ibeg iend is %d, %d, %d\n",idx,ibeg,iend);
 	
 
-	for (k = 0; k < Nleaf; k++){
+	// for (k = 0; k < Nleaf; k++){
 		ibeg = leafarr[0][k];
 		nrow = leafarr[1][k];
 		iend = leafarr[2][k];
-		printf("ibeg nrow iend is %d, %d, %d\n",ibeg,nrow,iend);
+		// printf("ibeg nrow iend is %d, %d, %d\n",ibeg,nrow,iend);
 
 	// Kokkos::parallel_for("psolve", Nleaf, KOKKOS_LAMBDA(int k) {
 		// double tp[3], tq[3], sp[3], sq[3];
@@ -606,8 +606,8 @@ int *psolve(double *z, double *r) {
 
     	//printf("%d %d %d %d\n", idx, ibeg, iend, nrow);
 
-    	// idx += nrow;
-
+    	idx += nrow;
+    	k += 1;
   	// }
     // });
 	}
