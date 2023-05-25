c70e47059aaa2b7e3105ff958731c01253a6aded
Sharon Yang <xiny@smu.edu>
2023-05-25 11:43:23 -0500

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -424,7 +424,7 @@ int *psolve(double *z, double *r) {
 	    leafarr[2][arridx] = nrow;
 	    // printf("nrow is: %d\n",leafarr[2][arridx]);
 	    // printf("arridx is: %d\n",arridx);
-	    printf("ibeg iend nrow: %d, %d, %d\n", &leafarr[0][arridx], &leafarr[1][arridx], &leafarr[2][arridx] );
+	    printf("ibeg iend nrow: %d, %d, %d\n", leafarr[0][arridx], leafarr[1][arridx], leafarr[2][arridx] );
 		// printf("ibeg iend nrow is %d, %d, %d\n",ibeg,iend,nrow);
 		arridx += 1;
 		Nleafc += 1;
