13c66d0b25e190b4e93a3d995b39add9c3df221d
Sharon Yang <xiny@smu.edu>
2023-05-28 18:22:36 -0500

    Update env_kokkos.h

--- kokkos/env_kokkos.h
+++ kokkos/env_kokkos.h
@@ -1,15 +1,16 @@
 #ifndef ENV_KOKKOS
 #define ENV_KOKKOS
 
-    typedef Kokkos::Serial   HostExecSpace;
-    typedef Kokkos::Cuda     DevExecSpace;
-    typedef Kokkos::CudaSpace    MemSpace;
-    typedef Kokkos::LayoutRight  Layout;
-    typedef Kokkos::RangePolicy<HostExecSpace>  host_range_policy;
-  	typedef Kokkos::RangePolicy<DevExecSpace>   dev_range_policy;
+#include <Kokkos_Core.hpp>
+typedef Kokkos::Serial   HostExecSpace;
+typedef Kokkos::Cuda     DevExecSpace;
+typedef Kokkos::CudaSpace    MemSpace;
+typedef Kokkos::LayoutRight  Layout;
+typedef Kokkos::RangePolicy<HostExecSpace>  host_range_policy;
+typedef Kokkos::RangePolicy<DevExecSpace>   dev_range_policy;
 
-  	typedef Kokkos::View<double*, Layout, MemSpace>   ViewVectorType;
-    typedef Kokkos::View<double**, Layout, MemSpace>  ViewMatrixType;
+typedef Kokkos::View<double*, Layout, MemSpace>   ViewVectorType;
+typedef Kokkos::View<double**, Layout, MemSpace>  ViewMatrixType;
 
 
 
