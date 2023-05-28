fd6ab3dbd8199cc96539b2bd688faa65e5335a2a
Sharon Yang <xiny@smu.edu>
2023-05-28 18:24:10 -0500

    c

--- kokkos/main_kokkos.cpp
+++ kokkos/main_kokkos.cpp
@@ -149,7 +149,7 @@ int main(int argc, char *argv[]) {
 	// free(tr_q);
 
 	// free(tr_area);
-	free(bvct);
+	// free(bvct);
 	// free(xvct);
 	// free(atmchr);
 	// free(chrpos);
@@ -158,7 +158,7 @@ int main(int argc, char *argv[]) {
 	Kokkos::kokkos_free(tr_q);
 
 	Kokkos::kokkos_free(tr_area);
-	// Kokkos::kokkos_free(bvct);
+	Kokkos::kokkos_free(bvct);
 	Kokkos::kokkos_free(xvct);
 
 
