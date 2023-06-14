088b454710903439cd3b6ebc3ced96f11a199350
Sharon Yang <xiny@smu.edu>
2023-06-13 23:05:35 -0500

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -11,7 +11,7 @@
 
 #include "utilities.h"
 #include "tree_node_struct.h"
-// #include "gl_constants.h"
+#include "gl_constants.h"
 // #include "gl_variables.h"
 // #include "env_kokkos.h"
 // #include <Kokkos_Core.hpp>
@@ -29,20 +29,20 @@ extern double *work, *h;
 
 extern double **tr_xyz2D, **tr_q2D;
 
-extern double pi;
-extern double one_over_4pi;
-extern double bulk_coef;
-extern double units_coef;
-extern double epsw;
-extern double epsp;
-extern double eps;
-extern double bulk_strength;	//ion_strength in M
-extern double kappa2;	//kappa2=bulk_coef*bulk_strength/epsw;
-extern double kappa;
+// extern double pi;
+// extern double one_over_4pi;
+// extern double bulk_coef;
+// extern double units_coef;
+// extern double epsw;
+// extern double epsp;
+// extern double eps;
+// extern double bulk_strength;	//ion_strength in M
+// extern double kappa2;	//kappa2=bulk_coef*bulk_strength/epsw;
+// extern double kappa;
 
-extern int order;
-extern int maxparnode;
-extern double theta;
+// extern int order;
+// extern int maxparnode;
+// extern double theta;
 
 extern double** Make2DDoubleArray(int arraySizeX, int arraySizeY, char info[]);
 extern int** Make2DIntArray(int arraySizeX, int arraySizeY,char info[]);
