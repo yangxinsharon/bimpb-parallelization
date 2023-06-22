9a8694e673a32ba0ca5463801bf1a41916b22fe6
Sharon Yang <xiny@smu.edu>
2023-06-21 23:58:55 -0500

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -487,7 +487,7 @@ void psolvemul(int nface, double *tr_xyz, double *tr_q, double *tr_area,
 
       	// });
 
-	 	Kokkos::fence();
+	 	// Kokkos::fence();
     	for ( i = 0; i < nrow; i++) {
       		rhs[i] = r[i+ibeg];
       		rhs[i+nrow] = r[i+ibeg+nface];
