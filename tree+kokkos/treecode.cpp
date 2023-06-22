33ecc1771cd58395940cc4f311b73d2d8c641e98
Sharon Yang <xiny@smu.edu>
2023-06-21 23:56:06 -0500

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -351,19 +351,20 @@ void psolvemul(int nface, double *tr_xyz, double *tr_q, double *tr_area,
 		int iend = leafarr[2+3*k];
 		int nrow2 = nrow*2;
 
+		int inc;
+		double L1, L2, L3, L4, area;
+		double tp[3], tq[3], sp[3], sq[3];
+		double r_s[3], rs, irs, sumrs;
+		double G0, kappa_rs, exp_kappa_rs, Gk;
+		double cos_theta, cos_theta0, tp1, tp2, dot_tqsq;
+		double G10, G20, G1, G2, G3, G4;
+		
 	 	// Kokkos::parallel_for("psolvemul2", 10, KOKKOS_LAMBDA(int i){
     	for ( i = ibeg; i <= iend; i++ ) {
-		  	int j,inc;
-		  	int nrow2 = nrow*2;
-		  	int nrow = leafarr[1+3*k];
-
-		  	double L1, L2, L3, L4, area;
-		  	double tp[3], tq[3], sp[3], sq[3];
-		  	double r_s[3], rs, irs, sumrs;
-		  	double G0, kappa_rs, exp_kappa_rs, Gk;
-		  	double cos_theta, cos_theta0, tp1, tp2, dot_tqsq;
-		  	double G10, G20, G1, G2, G3, G4;
-
+			// int j,inc;
+			// int nrow2 = nrow*2;
+			// int nrow = leafarr[1+3*k];
+			
     		// tp[0] = tr_xyz2D[0][i];
 			// tp[1] = tr_xyz2D[1][i];
 			// tp[2] = tr_xyz2D[2][i];
