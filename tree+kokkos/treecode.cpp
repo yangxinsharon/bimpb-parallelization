258cfcafcf8b7e69cc39d83a68039b2c7f8fcc22
Sharon Yang <xiny@smu.edu>
2023-06-25 14:56:13 -0500

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -358,20 +358,19 @@ void psolvemul(int nface, double *tr_xyz, double *tr_q, double *tr_area,
 	// Kokkos::View<double**, Kokkos::HostSpace> matrixA_dev("matrixA_dev",2*maxparnode,2*maxparnode);
 
 
-	// ViewMatrixDouble matrixA_dev("matrixA_dev",2*maxparnode,2*maxparnode);
-	// ViewMatrixDouble::HostMirror matrixA_h = Kokkos::create_mirror_view( matrixA_dev );
-  	// for (int i =0; i<2*maxparnode; i++){
-  	// 	for (int j =0; j<2*maxparnode; j++){
-  	// 		matrixA_h( i,j ) = 0;
-  	// 	}
-  	// }
-	// Kokkos::fence();
+	ViewMatrixDouble matrixA_dev("matrixA_dev",2*maxparnode,2*maxparnode);
+	ViewMatrixDouble::HostMirror matrixA_h = Kokkos::create_mirror_view( matrixA_dev );
+  	for (int i =0; i<2*maxparnode; i++){
+  		for (int j =0; j<2*maxparnode; j++){
+  			matrixA_h( i,j ) = 0;
+  		}
+  	}
+	Kokkos::fence();
 
-	// Kokkos::deep_copy( matrixA_dev, matrixA_h );
+	Kokkos::deep_copy( matrixA_dev, matrixA_h );
 	// for (int k=0; k<arridx; k++){
 	Kokkos::parallel_for("psolvemul", dev_range_policy(0,arridx), KOKKOS_LAMBDA(int k) {
 	  	// printf("matrixA_dev(0,0) is %f\n", matrixA_dev(0,0));
-	  	ViewMatrixDouble matrixA_dev("matrixA_dev",2*maxparnode,2*maxparnode);
 	  	int i,j,inc;
   		double L1, L2, L3, L4, area;
   		double tp[3], tq[3], sp[3], sq[3];
