b1bf00da5772b309725fef6df34ccf4feff30502
Sharon Yang <xiny@smu.edu>
2023-07-13 19:22:21 -0500

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -389,7 +389,7 @@ void psolvemul(int nface, double *tr_xyz, double *tr_q, double *tr_area,
 		int nrow = leafarr[1+3*k];
 		int iend = leafarr[2+3*k];
 		int nrow2 = nrow*2;
-		double matrixA1DD[10000];
+		double *matrixA1DD;
   		printf("nrow maxparnode k %d %d %d\n",nrow, maxparnode, k);
 	 	// print k; 0707
     	for ( i = ibeg; i <= iend; i++ ) {
