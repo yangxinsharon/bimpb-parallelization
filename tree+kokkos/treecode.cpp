ff5aacf8d10e8659b93c54c5598a4105b0931197
Sharon Yang <xiny@smu.edu>
2023-10-31 13:53:34 -0500

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -408,7 +408,7 @@ void psolvemul(int nface, double *tr_xyz, double *tr_q, double *tr_area,
   	pre1 = 0.5*(1.0+eps);
   	pre2 = 0.5*(1.0+1.0/eps);
 
-	// timer_start((char*) "psolve time");
+	timer_start((char*) "psolve time");
 	// Kokkos::View<double**, Kokkos::HostSpace> matrixA_dev("matrixA_dev",2*maxparnode,2*maxparnode);
 
 
@@ -753,7 +753,7 @@ void psolvemul(int nface, double *tr_xyz, double *tr_q, double *tr_area,
 	
 
 	Kokkos::fence();
-	// timer_end();
+	timer_end();
   	// printf("Nleafc is %d\n",Nleafc);
 
 }
