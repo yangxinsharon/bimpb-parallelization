c8385c5d7895d76cb13cf551ad696689e809803e
Sharon Yang <xiny@smu.edu>
2023-11-08 21:51:25 -0600

    c

--- tree+kokkos/env_kokkos.h
+++ tree+kokkos/env_kokkos.h
@@ -16,3 +16,25 @@ typedef Kokkos::View<int**, Layout, MemSpace>  ViewMatrixInt;
 #endif
 
 // #if defined(USE_UVM)
+
+//   // set the Kokkos execution space and range policy
+// #if defined(USE_OPENMP)
+//   typedef Kokkos::OpenMP     ExecSpace;
+//   typedef Kokkos::HostSpace  MemSpace;
+//   std::cout << "using Kokkos with OpenMP backend:\n";
+// #elif defined(USE_CUDA)
+//   typedef Kokkos::Cuda       ExecSpace;
+//   typedef Kokkos::CudaSpace  MemSpace;
+//   std::cout << "using Kokkos with CUDA backend:\n";
+// #elif defined(USE_UVM)
+//   typedef Kokkos::Cuda          ExecSpace;
+//   typedef Kokkos::CudaUVMSpace  MemSpace;
+//   std::cout << "using Kokkos with CUDA backend:\n";
+// #else
+//   typedef Kokkos::Serial     ExecSpace;
+//   typedef Kokkos::HostSpace  MemSpace;
+//   std::cout << "using Kokkos with Serial backend:\n";
+// #endif
+//   typedef Kokkos::RangePolicy<ExecSpace>            RangePol;
+//   typedef Kokkos::View<double*, MemSpace>           VecViewDev;
+//   typedef Kokkos::View<double*, Kokkos::HostSpace>  VecViewHost;
