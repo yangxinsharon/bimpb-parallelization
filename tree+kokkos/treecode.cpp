3ca10678e6f6c9c8d6e42e2660a4fabaea53b6b6
Sharon Yang <xiny@smu.edu>
2023-11-20 11:48:52 -0600

    Update treecode.cpp

--- tree+kokkos/treecode.cpp
+++ tree+kokkos/treecode.cpp
@@ -314,7 +314,7 @@ void psolvemul(int nface, double *tr_xyz, double *tr_q, double *tr_area,
 
 	// timer_start((char*) "psolve time");
 	// Kokkos::Timer timer;
-	Kokkos::parallel_for("psolvemul", host_range_policy(0,arridx), KOKKOS_LAMBDA(int k) {
+	Kokkos::parallel_for("psolvemul", dev_range_policy(0,arridx), KOKKOS_LAMBDA(int k) {
 	// Kokkos::parallel_for("psolvemul", team_policy(arridx,Kokkos::AUTO), KOKKOS_LAMBDA(const member_type &team_member) {
 	  	// int k = team_member.league_rank () * team_member.team_size () +team_member.team_rank ();
 	  	// int k = team_member.league_rank(); // total rank = 128
